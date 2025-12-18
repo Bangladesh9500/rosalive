50abea0ec7bf8e9f817cbcc6e3695c2b70dccd25
Md Ashik <ecolive50@gmail.com>
2025-12-18 11:39:07 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
-import 'live_room_page.dart';
+import 'package:zego_uikit_prebuilt_live_streaming/zego_uikit_prebuilt_live_streaming.dart';
 
 void main() {
-  runApp(const RosaLive());
+  runApp(const MyApp());
 }
 
-class RosaLive extends StatelessWidget {
-  const RosaLive({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LiveRoomPage(),
+      title: 'Rosa Live',
+      home: const LiveRoomPage(),
+    );
+  }
+}
+
+class LiveRoomPage extends StatelessWidget {
+  const LiveRoomPage({super.key});
+
+  // 🔴 ZEGOCLOUD INFO (CHANGE THIS)
+  final int appID = 123456789; // 👉 your Zego AppID
+  final String appSign = 'YOUR_APP_SIGN_HERE';
+
+  @override
+  Widget build(BuildContext context) {
+    return ZegoUIKitPrebuiltLiveStreaming(
+      appID: appID,
+      appSign: appSign,
+      userID: 'user_${DateTime.now().millisecondsSinceEpoch}',
+      userName: 'Guest',
+      liveID: 'rosalive_room_1',
+      config: ZegoUIKitPrebuiltLiveStreamingConfig.host(),
     );
   }
 }
