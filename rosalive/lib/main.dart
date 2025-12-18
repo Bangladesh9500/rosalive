a868b9aad2191dc70a8a09fcaab8e2bfab8f443b
Md Ashik <ecolive50@gmail.com>
2025-12-18 11:20:52 +0600

    Initialize RosaLive Flutter application

---
+++ rosalive/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'screens/live_room.dart';
+
+void main() {
+  runApp(const RosaLive());
+}
+
+class RosaLive extends StatelessWidget {
+  const RosaLive({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LiveRoomPage(),
+    );
+  }
+}
